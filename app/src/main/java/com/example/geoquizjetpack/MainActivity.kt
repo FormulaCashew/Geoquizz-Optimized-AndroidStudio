07b51338c4d38f6ac91a70a88a6fad7c6f74f3c1
FormulaCashew <acerty19@gmail.com>
2025-11-04 12:31:14 -0600

    Added preview function

--- app/src/main/java/com/example/geoquizjetpack/MainActivity.kt
+++ app/src/main/java/com/example/geoquizjetpack/MainActivity.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -50,6 +51,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyScreen(){
     HandleOrientationChanges()
+}
+
+@Preview
+@Composable
+fun Preview(){
+    MyScreen()
 }
 
 @Composable
